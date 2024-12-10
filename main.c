d1b8000922a24ebbf3e846731b70873a589e413c
lenanya <lenashimizu@gmx.net>
2024-12-10 19:48:38 +0100

    just realized it doesnt even work

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #define uint1 bool
-#define SIZE 3
+#define SIZE 10
 #define wrapping_mod(a, b) ((a % b + b) % b)
 
 void print_field(uint1 *field, size_t size) {
@@ -12,7 +14,7 @@ void print_field(uint1 *field, size_t size) {
             if (field[y * SIZE + x]) {
                 printf("O");
             } else {
-                printf(" ");
+                printf(".");
             }
         }
         printf("\n");
@@ -24,41 +26,43 @@ void eval(uint1 *field, size_t size) {
     for (int y = 0; y < size; ++y) {
         for (int x = 0; x < size; ++x) {
             size_t neighbors = 0;
-            for (int i = -1; i <= 1; ++i) {
-                if (!i) continue;
-                if (field[y * size + wrapping_mod(x + i, size)]) {
-                    neighbors++;
+            for (int yi = -1; yi <= 1; ++yi) {
+                for (int xi = -1; xi <= 1; ++xi) {
+                    if (!xi && !yi) continue;
+                    if (field[wrapping_mod(y + yi, size) * size + wrapping_mod(x + xi, size)]) {
+                        neighbors++;
+                    }
                 }
+                
             }
-            for (int i = -1; i <= 1; ++i) {
-                if (!i) continue; 
-                if (field[wrapping_mod(y + i, size) * size + x]) {
-                    neighbors++;
-                }
-            }
+            
+            printf("%d", neighbors);
             if (neighbors < 2) {
                 field[y * size + x] = 0;
             } else if (neighbors > 3) {
                 field[y * size + x] = 0;
-            } else {
+            } else if (neighbors == 2 || neighbors == 3) {
                 field[y * size + x] = 1;
             }
         }
+        printf("\n");
     }
 }
 
 int main(void) {
 
-    uint1 field[SIZE * SIZE] = {
-        0, 1, 0,
-        0, 0, 1,
-        1, 1, 1,
-    };
+    uint1 *field = calloc(sizeof(uint1), SIZE*SIZE); 
+    
+    field[99] = 1;
+    field[98] = 1;
+    field[97] = 1;
+    field[89] = 1;
+    field[78] = 1;
 
     for (;;) {
         print_field(field, SIZE);
         eval(field, SIZE);
-        getchar();
+        usleep(100000);
     }
 
     return 0;
